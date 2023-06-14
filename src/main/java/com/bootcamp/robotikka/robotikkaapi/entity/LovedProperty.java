083f5ce922ec516b4926121c4a30aed90f043c3d
Abeetha <abeethaabeysundera@gmail.com>
2023-06-14 09:21:08 +0530

    Variable Declaration

---
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/entity/LovedProperty.java
@@ -0,0 +1,10 @@
+package com.bootcamp.robotikka.robotikkaapi.entity;
+
+import java.util.Date;
+
+public class LovedProperty {
+    private String propertyId;
+    private Date savedDate;
+  /*  user_property_id;
+    product_property_id;*/
+}
