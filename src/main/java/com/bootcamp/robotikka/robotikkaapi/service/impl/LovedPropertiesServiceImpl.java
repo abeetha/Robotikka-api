de9405d9fa23d1a93c06901a66010d92b1f0e843
Abeetha <abeethaabeysundera@gmail.com>
2023-06-15 12:04:04 +0530

    Controller Created

---
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/service/impl/LovedPropertiesServiceImpl.java
@@ -0,0 +1,6 @@
+package com.bootcamp.robotikka.robotikkaapi.service.impl;
+
+import com.bootcamp.robotikka.robotikkaapi.service.LovedPropertiesService;
+
+public class LovedPropertiesServiceImpl implements LovedPropertiesService {
+}
---
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/service/impl/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package com.bootcamp.robotikka.robotikkaapi.service.impl;
+
+
+import com.bootcamp.robotikka.robotikkaapi.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+}
