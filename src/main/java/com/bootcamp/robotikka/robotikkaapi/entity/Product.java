eb6f9c2ef5a22c9802492c4b80e205da5b0bf640
Abeetha <abeethaabeysundera@gmail.com>
2023-06-19 16:53:14 +0530

    Controller Created

--- src/main/java/com/bootcamp/robotikka/robotikkaapi/entity/Product.java
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/entity/Product.java
@@ -16,19 +16,26 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+
     @Id
     @Column(name = "property_id")
     private String propertyId;
+
     @Column(name = "display_name")
     private String displayName;
+
     private String description;
+
     @Column(name = "unit_price")
     private double unitPrice;
     private int qty;
+
     @OneToMany(mappedBy = "product",cascade = CascadeType.ALL)
     private List<ProductImages> images;
+
     @Column(name = "selling_price")
     private double sellingPrice;
+
     @OneToMany(mappedBy = "productPropertyId",cascade = CascadeType.ALL)
     private List<Orders> orderList;
 
